f6b90e530d7856a3720df488464713ebf18772b1
Zunaira <zunairaabid140@gmail.com>
2025-12-22 14:42:15 +0500

    Store error

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,9 @@
 import cartReducer from "../features/cart/cartSlice";
 import productReducer from "../features/cart/ProductSlice";
 import { configureStore } from "@reduxjs/toolkit";
-import wishlistReducer from "../features/cart/WishListSlice";
-import listViewReducer from "../features/cart/listViewSlice";
-import productsReducer from "../features/products/productsSlice";
+import wishlistReducer from "../features/cart/wishlistSlice.js";
+import listViewReducer from "../features/cart/listViewSlice.js";
+import productsReducer from "../features/products/productsSlice.js";
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
